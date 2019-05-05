75a8849cbec6b087910c40a69b38c7b407716802
Emily Ring <btyy77c@gmail.com>
2019-05-05 08:01:52 -0400

    Category Mostly working with session

--- models/category.py
+++ models/category.py
@@ -7,8 +7,30 @@ from setup import CategoryTable
 from session import Session
 
 session = Session()
+database_table = CategoryTable
 
 class Category:
+    def __init__(self, name, id):
+        self.name = name
+        self.id = id
+
     @classmethod
-    def AllCategories(cls):
-        return session.query(CategoryTable).all()
+    def all(cls):
+        db_categories = session.query(database_table).all()
+        categories = []
+        for db_category in db_categories:
+            categories.append(cls(db_category.name, db_category.id))
+        return categories
+
+    def delete():
+        db_object = session.query(database_table).filter_by(id = self.id).one()
+        session.delete(db_object)
+        session.commit()
+        return 'FINIsh me!!'
+
+    def save():
+        db_object = session.query(database_table).filter_by(id = self.id).one()
+        db_object.name self.name
+        session.add(db_object)
+        session.commit()
+        return ' Finish ME!!!'
