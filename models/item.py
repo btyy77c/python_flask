ad56615031f169661c636568d7ff97467b039bac
Emily Ring <btyy77c@gmail.com>
2019-05-22 17:12:22 -0400

    Added comments to item file

--- models/item.py
+++ models/item.py
@@ -1,12 +1,18 @@
-import os, sys
+import os
+import sys
 from os.path import dirname, join, abspath
+import datetime
 sys.path.insert(0, abspath(join(dirname(__file__), '../database')))
 
-import datetime
-from setup import ItemTable
+try:
+    from setup import ItemTable
+except ImportError:
+    raise
+
 
 class ItemModel:
     def __init__(self, values_hash):
+        """Creates item with values match database object"""
         self.id = values_hash.get('id', None)
         self.errors = values_hash.get('errors', None)
         self.category_id = values_hash.get('category_id', None)
@@ -17,9 +23,11 @@ class ItemModel:
         self.updated_date = values_hash.get('updated_date', None)
 
     def __not_null_attributes(self):
-        return { k: v for k, v in self.attributes().items() if v is not None }
+        """serialized attributes, but removed None key/values"""
+        return {k: v for k, v in self.attributes().items() if v is not None}
 
     def attributes(self):
+        """serialized attributes that can be used for json requests"""
         return {
             'id': self.id,
             'errors': self.errors,
@@ -32,40 +40,49 @@ class ItemModel:
         }
 
     def create(self, session):
-        if self.id == None:
+        """adds new object to the database"""
+        if self.id is None:
+            """add object to database"""
             db_object = ItemTable(**self.__not_null_attributes())
             session.add(db_object)
             session.commit()
             self.id = db_object.id
             return self
         else:
+            """if object has id, run update instead of create"""
             return self.update(session)
 
     def delete(self, session):
-        db_object = session.query(ItemTable).filter_by(title = self.title).one()
+        """deletes object from database"""
+        db_object = session.query(ItemTable).filter_by(title=self.title).one()
 
         if self.created_by == db_object.created_by:
             session.delete(db_object)
             session.commit()
         else:
+            """does not process deletion if created_by is changed"""
             self.errors = 'Only ' + db_object.created_by + ' can delete'
         return self
 
     def update(self, session):
-        db_objects = session.query(ItemTable).filter_by(id = self.id)
+        """update objects in database"""
+        db_objects = session.query(ItemTable).filter_by(id=self.id)
         db_object = db_objects.one()
 
-        if db_object.created_by != self.created_by:
-            self.errors = 'Only ' + db_object.created_by + ' can update'
-        else:
+        if db_object.created_by == self.created_by:
             self.updated_date = datetime.datetime.now()
             db_objects.update(self.__not_null_attributes())
             session.commit()
+        else:
+            """does not process update if created_by is changed"""
+            self.errors = 'Only ' + db_object.created_by + ' can update'
+
         return self
 
     @classmethod
     def category_group(cls, session, category_id):
-        db_items = session.query(ItemTable).filter_by(category_id = category_id)
+        """query all items related to a single category"""
+        db_items = session.query(ItemTable).filter_by(category_id=category_id)
         items = []
         for db_item in db_items:
             items.append(cls(db_item.serialize()))
@@ -73,22 +90,24 @@ class ItemModel:
 
     @classmethod
     def delete_category_group(cls, category_id, session):
-        db_items = session.query(ItemTable).filter_by(category_id = category_id)
+        """delete all items related to a single category"""
+        db_items = session.query(ItemTable).filter_by(category_id=category_id)
         for db_item in db_items:
             session.delete(db_item)
         return db_items
 
     @classmethod
     def find(cls, session, title):
-        try:
-            db_object = session.query(ItemTable).filter_by(title = title).one()
-            return cls(db_object.serialize())
-        except:
-            cls({ 'errors': 'Category Not Found' })
+        """find one item"""
+        db_object = session.query(ItemTable).filter_by(title=title).one()
+        return cls(db_object.serialize())
 
     @classmethod
     def latest(cls, session):
-        db_items = session.query(ItemTable).order_by(ItemTable.created_date.desc()).limit(10)
+        """find 10 most recent items"""
+        db_items = session.query(ItemTable).order_by(
+            ItemTable.created_date.desc()
+        ).limit(10)
         items = []
         for db_item in db_items:
             items.append(cls(db_item.serialize()))
