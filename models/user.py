92097bc17fda2b4268941f3e9f2de28a966386ab
Emily Ring <btyy77c@gmail.com>
2019-05-22 16:33:29 -0400

    Added comments to user model

--- models/user.py
+++ models/user.py
@@ -2,14 +2,16 @@ import firebase_admin
 from firebase_admin import auth
 from firebase_admin import credentials
 
-cred = credentials.Certificate("models/creds.json")
+cred = credentials.Certificate('creds.json')
 firebase_admin.initialize_app(cred)
+
 
 class UserModel:
     def __init__(self, token):
+        """Decodes jwt token to create a user with an email and name"""
         try:
             decoded_token = auth.verify_id_token(token)
-        except:
+        except ValueError:
             decoded_token = {}
         self.email = decoded_token.get('email', None)
         self.name = decoded_token.get('name', None)
