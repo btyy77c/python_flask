6bbf46ccff3b8278c59e5d5abfd31def9298333f
Emily Ring <btyy77c@gmail.com>
2019-05-22 17:23:57 -0400

    Added setup comments

--- database/setup.py
+++ database/setup.py
@@ -8,16 +8,28 @@ import datetime
 
 Base = declarative_base()
 
+
 class CategoryTable(Base):
+    """Category database table with values and serialize method"""
     __tablename__ = 'categories'
 
     id = Column(Integer, primary_key=True)
-    created_by = Column(String(250), CheckConstraint('length(created_by) >= 1'), nullable=False)
-    name = Column(String(250), CheckConstraint('length(name) >= 1'), nullable=False, unique=True)
-    created_date = Column(DateTime, nullable=False, default=datetime.datetime.now())
-    updated_date = Column(DateTime, nullable=False, default=datetime.datetime.now())
+    created_by = Column(
+        String(250), CheckConstraint('length(created_by) >= 1'), nullable=False
+    )
+    name = Column(
+        String(250), CheckConstraint('length(name) >= 1'),
+        nullable=False, unique=True
+    )
+    created_date = Column(
+        DateTime, nullable=False, default=datetime.datetime.now()
+    )
+    updated_date = Column(
+        DateTime, nullable=False, default=datetime.datetime.now()
+    )
 
     def serialize(self):
+        """serialized list of database values"""
         return {
             'created_by': self.created_by,
             'created_date': self.created_date,
@@ -28,18 +40,32 @@ class CategoryTable(Base):
 
 
 class ItemTable(Base):
+    """Item database table with values and serialize method"""
     __tablename__ = 'items'
 
     id = Column(Integer, primary_key=True)
-    category_id = Column(Integer, ForeignKey('categories.id'), CheckConstraint('length(category_id) >= 1'), nullable=False)
+    category_id = Column(
+        Integer, ForeignKey('categories.id'),
+        CheckConstraint('length(category_id) >= 1'), nullable=False
+    )
     category = relationship(CategoryTable)
-    created_by = Column(String(250), CheckConstraint('length(created_by) >= 1'), nullable=False)
-    created_date = Column(DateTime, nullable=False, default=datetime.datetime.now())
+    created_by = Column(
+        String(250), CheckConstraint('length(created_by) >= 1'), nullable=False
+    )
+    created_date = Column(
+        DateTime, nullable=False, default=datetime.datetime.now()
+    )
     description = Column(String(250), nullable=False, default='')
-    title = Column(String(250), CheckConstraint('length(title) >= 1'), nullable=False, unique=True)
-    updated_date = Column(DateTime, nullable=False, default=datetime.datetime.now())
+    title = Column(
+        String(250), CheckConstraint('length(title) >= 1'),
+        nullable=False, unique=True
+    )
+    updated_date = Column(
+        DateTime, nullable=False, default=datetime.datetime.now()
+    )
 
     def serialize(self):
+        """serialized list of database values"""
         return {
             'id': self.id,
             'category_id': self.category_id,
